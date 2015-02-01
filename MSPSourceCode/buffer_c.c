78e3903a958f1e55a9fdac88416e7acf0ce02bd7
xinalei <christina.leichtenschlag@gmail.com>
2015-01-31 21:54:47 -0500

    Updated MSP source; sending "scan" to wifly now returns parsed data (comma-delimited list of network # and SSID per line)

--- MSPSourceCode/buffer_c.c
+++ MSPSourceCode/buffer_c.c
@@ -6,41 +6,35 @@
 //  NOTE: if some jack*** has a wifi network with "END:" in the name, this code will break. Highly unlikely, but yeah.
 
 // Function declarations
-int getCount(char *);
+int getCount(char []);
 
 #include <stdio.h>
 #include <stdlib.h> // malloc
 #include <string.h> // strlen
 
 #define NETWORKLEN 75
-int gotE=0,gotEN=0,gotEND=0,endofscan=0;
+int gotE=0,gotEN=0,gotEND=0,endofscan=0,inputLength=0;
 
-void parse(char* input, char **output_str) {
-    
+void parse(char input[], char output[]) {
+
+	inputLength = (int)strlen(input);
+	if(0==inputLength) return;
+
     // Sample test input.
-    //char *testinp1 = "SCAN:Found 13\n01,01,-59,04,1104,28,c0,20:4e:7f:08:df:85,dad-rules\n02,03,-64,02,1104,28,00,00:30:bd:9b:49:22,basement\n03,10,-71,04,3100,28,00,90:27:e4:5d:fc:a7,URSOMONEY\n04,10,-71,04,3100,28,00,90:27:e4:5d:fc:a7,networkkkk!\nEND:";
     char x = *input;
     char *ptr = input;
-    char *ptr_out;
-    int numNetworks = getCount(input);
-    
-    *output_str = (char*) malloc(numNetworks*NETWORKLEN*sizeof(char));
-    if(NULL == *output_str)
-    {
-        return;
-    }
-    ptr_out = *output_str;
-    
-    //Look for "SCAN"
+    char *ptr_out = output;
+
+    // Look for "SCAN"
     while('S'!= *ptr
-            || 'C'!= *(ptr+1)
-            || 'A'!= *(ptr+2)
-            || 'N'!= *(ptr+3))
+    		&& 'C'!= *(ptr+1)
+    		&& 'A'!= *(ptr+2)
+    		&& 'N'!= *(ptr+3))
     {
-        ptr++;
+    	ptr++;
     }
-    
-    // Found "SCAN". Now, ignore this line (tells you how many networks found)
+
+    // Found "SCAN", now ignore this line (tells you how many networks found)
     while('\n' != x)
     {
         // Update x
@@ -50,9 +44,9 @@ void parse(char* input, char **output_str) {
     // Move to the character after the newline.
     ptr++;
     x=*ptr;
-    
+
     int gotListNum=0,commaCount=0,hitSSID=0;
-    
+
     // While loop; continue looking at letters until we find "END:".
     while('\0' != x)
     {
@@ -76,7 +70,7 @@ void parse(char* input, char **output_str) {
                 {
                     gotE = 0; // not the end.
                     *ptr_out = 'E'; // put back 'E'
-                    ptr_out++;
+					ptr_out++;
                 }
             }
             else // got "EN"
@@ -91,9 +85,9 @@ void parse(char* input, char **output_str) {
                     {
                         gotE = gotEN = 0; // not the end.
                         *ptr_out = 'E'; // put back "EN"
-                        ptr_out++;
-                        *ptr_out = 'N';
-                        ptr_out++;
+						ptr_out++;
+						*ptr_out = 'N';
+						ptr_out++;
                     }
                 }
                 else // got "END"
@@ -107,16 +101,16 @@ void parse(char* input, char **output_str) {
                     {
                         gotE = gotEN = gotEND = 0; // not the end.
                         *ptr_out = 'E'; // put back "END"
-                        ptr_out++;
-                        *ptr_out = 'N';
-                        ptr_out++;
-                        *ptr_out = 'D';
-                        ptr_out++;
+						ptr_out++;
+						*ptr_out = 'N';
+						ptr_out++;
+						*ptr_out = 'D';
+						ptr_out++;
                     }
                 }
             }
         }
-        
+
         // If we get to here we haven't gotten to the end, because there's a break at the end.
         if((0==gotListNum || 1==gotListNum) && 0==gotE && 0==gotEN && 0==gotEND) // two digit list num.
         {
@@ -130,7 +124,7 @@ void parse(char* input, char **output_str) {
             ptr_out++;
             gotListNum++; // dont want to add too many commas!
         }
-        
+
         // Look at current character; where we at?
         if('\n' == x)
         {
@@ -157,52 +151,47 @@ void parse(char* input, char **output_str) {
                 ptr_out++;
             }
         }
-        
+
         // Update x
         ptr++;
         x=*ptr;
     }
-    
-    // So what happens is that the last 3 characters need to be removed.
-    //*output_str[(int)strlen(*output_str)-1] = '\0';
-    //free(output_str);
-    
-    //return *output_str;
 }
 
-int getCount(char *string)
+int getCount(char string[])
 {
-    char c = *string;
-    
+	int loc = 0;
+    char c = string[loc];
+
     /* After entering a scan command, the wifly module always returns the following
-     "SCAN: FOUND ... END:", where ... begins with an nonnegative integer indicating how many networks were found,
+     "SCAN: Found ... END:", where ... begins with an nonnegative integer indicating how many networks were found,
      followed by each network and a ton of data, and ending with "END".
      By looping until we find the first lowercase d, we're going to have a pointer just before the number of networks
      that were found.
      */
     while('d' != c)
     {
-        string++;
-        c = *string;
+        loc++;
+        c = string[loc];
     }
-    
+
     // c == 'd', thus next character is a space, then we get to the digits.
     // skip the space - increment pointer twice
-    string++;
-    string++;
-    c = *string;
+    loc++;
+    loc++;
+    c = string[loc];
     char digits[2] = {'\0','\0'};
     int i=0;
     while('\n' != c)
     {
         digits[i++] = c;
-        string++;
-        c = *string;
+        loc++;
+        c = string[loc];
         if(i>1) break;
     }
-    
+
     int ret = -1; // return value
-    
+
     if('\0' == digits[1])
     {
         // Single digit
@@ -215,5 +204,3 @@ int getCount(char *string)
     }
     return ret;
 }
-
-
---
+++ MSPSourceCode/main.c
@@ -0,0 +1,250 @@
+// Going to remove the interrupts (?)
+
+#include <msp430.h>
+#include <string.h>
+#include <stdlib.h>
+#include "buffer_c.h"
+
+#define BUFFER_SIZE 2000
+
+char wiflyBuffer[BUFFER_SIZE]; // about 75 chars per network found.
+char parsed[BUFFER_SIZE];
+int wiflyLastRec = 0;
+int scanning = 0; // boolean 0=false 1=true
+
+volatile int wiflyCommand = 0; // false
+
+int main(void)
+{
+	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+
+	// initialize A0 uart for comm with the wifly module
+	P3SEL = BIT3+BIT4;                        // P3.4,5 = USCI_A0 TXD/RXD
+	UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+	UCA0BR0 = 9;                              // 1MHz 115200 (see User's Guide)
+	UCA0BR1 = 0;                              // 1MHz 115200
+	UCA0MCTL |= UCBRS_1 + UCBRF_0;   // Modln UCBRSx=0, UCBRFx=0, over sampling
+	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+	// initialize A1 uart for comm with the bluetooth module
+	P4SEL |= BIT4+BIT5;                       // P4.4,5 = USCI_A1 TXD/RXD
+	UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
+	UCA1CTL1 |= UCSSEL_2;                     // SMCLK
+	UCA1BR0 = 9;                              // 1MHz 115200 (see User's Guide)
+	UCA1BR1 = 0;                              // 1MHz 115200
+	UCA1MCTL |= UCBRS_1 + UCBRF_0;   // Modln UCBRSx=0, UCBRFx=0,
+	UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+	UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
+
+	// And i guess from here we just keep calling other functions.?
+	// First thing that will happen should be the bluetooth module receiving commands
+	  // from the user/android device. First command should be to select a wifi network.
+	// > s : scan for a network
+
+	__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, interrupts enabled
+	__no_operation();                         // For debugger
+}
+
+
+// Send "$$$" to the Wifly module to put it into command mode. Should reply with "CMD".
+void wiflyEnterCommandMode()
+{
+	// Send "$$$"
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '$';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '$';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '$';
+}
+
+void wiflyExitCommandMode()
+{
+	// Send "exit\n\r"
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'e';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'x';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'i';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 't';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\n';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\r';
+}
+
+void wiflyScanCommand()
+{
+	// Send "scan\n\r"
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 's';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'c';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'a';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'n';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\n';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\r';
+}
+
+void wiflyRebootCommand()
+{
+	// Send "reboot\n\r"
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'r';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'e';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'b';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'o';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 'o';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = 't';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\n';
+	while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+	UCA0TXBUF = '\r';
+}
+
+void parseScanData()
+{
+//	while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+//		UCA1TXBUF = '~'; // Send char
+//		while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+//		UCA1TXBUF = '\n'; // Send char
+//		while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+//		UCA1TXBUF = '\r'; // Send char
+
+	// Pass the input to buffer_c.parse
+	char *ptr;
+	parse(wiflyBuffer,parsed);
+
+	// Now that we've got the parsed data, lets send it.
+	ptr = parsed;
+	while('\0' != *ptr)
+	{
+		while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+		UCA1TXBUF = *ptr; // Send char
+		ptr++;
+	}
+
+	// Send 0x04
+	while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+	UCA1TXBUF = 0x7F; // Send EOT
+	while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+	UCA1TXBUF = 0x7F; // Send EOT
+}
+
+
+// USCI_A0 interrupt -- wifly module
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+  case 0:break; // no interrupt
+  case 2: // RXIFG - received a character!
+
+	  if(1==scanning) // We're expecting scan data to be returned; put it in the buffer.
+	  {
+		  wiflyBuffer[wiflyLastRec++] = UCA0RXBUF; // put char into buffer.
+		  if(wiflyLastRec > BUFFER_SIZE-1)
+		  {
+			  wiflyLastRec = 0;
+		  }
+
+		  /// Checking if END has been received.
+		  if(wiflyLastRec > 2 && ('D'==wiflyBuffer[wiflyLastRec-1])
+							  && ('N'==wiflyBuffer[wiflyLastRec-2])
+							  && ('E'==wiflyBuffer[wiflyLastRec-3]) )
+		  {
+			  // IT WORKED. Received END after scanning
+
+			  // Parse the data & then send parsed data to Bluetooth.
+			  parseScanData();
+			  scanning = 0;
+
+			  // Reset buffer
+			  memset(wiflyBuffer,'\0',BUFFER_SIZE);
+			  memset(parsed,'\0',BUFFER_SIZE);
+			  wiflyLastRec = 0;
+		  }
+
+	  }
+	  else // Send the char received from Wifly to Bluetooth
+	  {
+		  // send to A1
+		  while (!(UCA1IFG&UCTXIFG)); // USCI_A1 TX buffer ready?
+		  UCA1TXBUF = UCA0RXBUF;
+	  }
+
+	  break;
+  case 4:break; // TXIFG - sending a character
+  default: break;
+  }
+}
+
+// USCI_A1 interrupt -- bluetooth module (or terminal if debugging)
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A1_VECTOR
+__interrupt void USCI_A1_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch(__even_in_range(UCA1IV,4))
+  {
+	case 0:break; // no interrupt
+	case 2: // RXIFG - received a character!
+
+		//scanning = 0; // Turn off logic that saves A0_RX characters into the buffer
+
+		if('W' == UCA1RXBUF) {
+			// we want to directly send commands to the wifly module from now on.
+			wiflyCommand = 1;
+			wiflyEnterCommandMode(); // send "$$$" to wifly
+		}
+		else if('E' == UCA1RXBUF) {
+			// stop sending commands to the wifly module.
+			wiflyCommand = 0;
+			wiflyExitCommandMode(); // send "exit\n\r" to wifly
+		}
+		else if('S' == UCA1RXBUF) {
+			scanning = 1;
+			wiflyScanCommand(); // send "scan\n\r" to wifly
+		}
+		else if('R' == UCA1RXBUF)
+		{
+			wiflyRebootCommand();
+		}
+		else {
+
+			if(1 == wiflyCommand) {
+				while (!(UCA0IFG&UCTXIFG)); // USCI_A0 TX buffer ready?
+				UCA0TXBUF = UCA1RXBUF; // send character to wifly module
+			}
+		}
+
+		break;
+	case 4:break; // TXIFG - sending a character
+	default: break;
+  }
+}
+
